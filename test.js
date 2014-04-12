4854bb7fd5173375a4366d633d995117ece8422d
Sam Hatfield <samuel.e.hatfield@gmail.com>
2014-04-11 19:42:57 -0500

    Started to make sense of the code more. It seems that my problem has to do with using the files without a server.

--- test.js
+++ test.js
@@ -1,22 +1,31 @@
-var linear = $('#source').createGradient({
-  x1: 0, y1: 0,
-  x2: 200, y2: 0,
-  c1: 'rgb(0, 0, 0)',
-  c2: '#0f0',
-  c3: 'blue',
-  c4: '#009c60',
-  c5: '#000099'
-});
+
 
 $(document).ready(function(){
-	$("#source").drawRect({
-		fillStyle: linear,
-		x: 0, y: 0,
-		width: 600,
-		height: 50,
-		fromCenter: false
+	var source = $("#source")[0];
+	var display = $("#display")[0];
+	var width = $("#source").css(width);
+	$("#source").drawLine({
+		strokeStyle: "#a55",
+		strokeWidth: 10,
+		x1: 0, y1: 0,
+		x2: 600, y2: 50,
 	});
+
 	$("#display").drawImage({
-		source: $("#source"),
+		source: source,
+		cropFromCenter: false,
+	});
+
+	$("#display").click(function(){
+	// iterate over the width of the source canvas
+
+		$.each(display, function (index, width){
+			$("#display").drawImage({
+				source: source,
+				x: index,
+				cropFromCenter: false,
+			});
+		})
 	});
 });
+
